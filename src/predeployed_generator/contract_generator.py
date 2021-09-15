1c64400d2e70ea3a207e9f15670b21a566472c50
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-09-15 16:44:30 +0300

    SKALE-4508 Fix linting warnings

--- src/predeployed_generator/contract_generator.py
+++ src/predeployed_generator/contract_generator.py
@@ -1,53 +1,85 @@
+'''contract_generator.py
+
+The module is used to generate predeployed smart contracts in genesis block
+
+Functions:
+    to_even_length
+    add_0x
+    calculate_mapping_value_slot
+    calculate_array_value_slot
+    next_slot
+
+Classes:
+    ContractGenerator
+'''
+
 from __future__ import annotations
 
 import json
-import os
-from web3 import Web3
+from web3.auto import w3
 
 
 def to_even_length(hex_string: str) -> str:
+    '''Modify hex string to have even amount of digits.
+
+    For example:
+        "0x0" becomes "0x00"
+        "0x123 becomes "0x0123"
+        "0x12" stays "0x12"
+    '''
     assert hex_string.startswith('0x')
     if len(hex_string) % 2 != 0:
         return "0x0" + hex_string[2:]
-    else:
-        return hex_string
+    return hex_string
 
 
 def add_0x(bytes_string: str) -> str:
+    '''Add "0x" prefix to the string'''
     if bytes_string.startswith('0x'):
         return bytes_string
-    else:
-        return '0x' + bytes_string
+    return '0x' + bytes_string
 
 
 def calculate_mapping_value_slot(slot: int, key: any, key_type: str) -> int:
+    '''Calculate slot in smart contract storage where value of the key in mapping is stored'''
     if key_type == 'address':
         return calculate_mapping_value_slot(slot, int(key, 16).to_bytes(32, 'big'), 'bytes32')
-    else:
-        return int.from_bytes(Web3.solidityKeccak([key_type, 'uint256'], [key, slot]), 'big')
+    return int.from_bytes(w3.solidityKeccak([key_type, 'uint256'], [key, slot]), 'big')
 
 
 def calculate_array_value_slot(slot: int, index: int) -> int:
-    return int.from_bytes(Web3.solidityKeccak(['uint256'], [slot]), 'big') + index
+    '''Calculate slot in smart contract storage where value of the array in the index is stored'''
+    return int.from_bytes(w3.solidityKeccak(['uint256'], [slot]), 'big') + index
 
 
 def next_slot(previous_slot: int) -> int:
+    '''Return next slot in smart contract storage'''
     return previous_slot + 1
 
 
 class ContractGenerator:
+    '''Generate smart contract allocation in a genesis block'''
     def __init__(self, bytecode: str, balance: int = 0, nonce: int = 0):
         self.bytecode = bytecode
         self.balance = balance
         self.nonce = nonce
         self.storage = {}
 
-    def from_hardhat_artifact(artifact_filename: str, balance: int = 0, nonce: int = 0) -> ContractGenerator:
-        with open(artifact_filename, encoding='utf-8') as fp:
-            contract = json.load(fp)
+    @staticmethod
+    def from_hardhat_artifact(
+            artifact_filename: str,
+            balance: int = 0,
+            nonce: int = 0) -> ContractGenerator:
+        '''Create ContractGenerator from the artifact file built by hardhat'''
+        with open(artifact_filename, encoding='utf-8') as artifact_file:
+            contract = json.load(artifact_file)
             return ContractGenerator(contract['deployedBytecode'], balance, nonce)
 
     def generate(self) -> dict:
+        '''Produce smart contract allocation object.
+
+        It consists of fields 'code', 'balance', 'nonce' and 'storage'
+        '''
         assert isinstance(self.bytecode, str)
         assert isinstance(self.storage, dict)
         assert isinstance(self.balance, int)
@@ -82,7 +114,9 @@ class ContractGenerator:
             members_value_slot = calculate_mapping_value_slot(members_slot, account, 'address')
             self._write_uint256(members_value_slot, 1)
             self._write_address(calculate_array_value_slot(values_slot, i), account)
-            self._write_uint256(calculate_mapping_value_slot(indexes_slot, int(account, 16), 'uint256'), i + 1)
+            self._write_uint256(
+                calculate_mapping_value_slot(indexes_slot, int(account, 16), 'uint256'),
+                i + 1)
 
     def _write_addresses_array(self, slot: int, values: list) -> None:
         self._write_uint256(slot, len(values))
--- src/predeployed_generator/upgradeable_contract_generator.py
+++ src/predeployed_generator/upgradeable_contract_generator.py
@@ -1,13 +1,27 @@
+'''upgradeable_contract_generator.py
+
+The module is used to generate predeployed smart contracts in genesis block
+that is upgradeable using EIP-1967 tranparent upgradeable proxy
+
+Classes:
+    UpgradeableContractGenerator
+'''
+
 from .contract_generator import ContractGenerator
 from .contracts.admin_upgradeability_proxy import AdminUpgradeabilityProxyGenerator
 
 
 class UpgradeableContractGenerator(AdminUpgradeabilityProxyGenerator):
-    def __init__(self, implementation_address: str, proxy_admin_address, implementation_generator: ContractGenerator):
+    def __init__(
+            self,
+            implementation_address: str,
+            proxy_admin_address,
+            implementation_generator: ContractGenerator):
         super().__init__(implementation_address, proxy_admin_address)
         self.implementation_generator = implementation_generator
 
     def generate_contract(self, balance: int = 0, nonce: int = 0) -> dict:
         contract = super().generate_contract(balance, nonce)
-        contract['storage'].update(self.implementation_generator.generate_contract(balance, nonce)['storage'])
+        contract['storage'].update(
+            self.implementation_generator.generate_contract(balance, nonce)['storage'])
         return contract
