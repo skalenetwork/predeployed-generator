89f91b82c4af53caeb535319ed7525440e6c2bd2
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-09-16 13:00:05 +0300

    SKALE-4508 Refactor ProxyAdminGenerator

--- src/predeployed_generator/openzeppelin/proxy_admin_generator.py
+++ src/predeployed_generator/openzeppelin/proxy_admin_generator.py
@@ -6,20 +6,26 @@ from ..contract_generator import ContractGenerator
 class ProxyAdminGenerator(ContractGenerator):
     ARTIFACT_FILENAME = 'ProxyAdmin.json'
 
+    OWNER_SLOT = 0
+
     # ---------- storage ----------
     # -----------Context-----------
     # -----------Ownable-----------
     # 0:    _owner
     # ---------ProxyAdmin----------
 
-    def __init__(self, owner_address: str):
+    def __init__(self):
         artifacts_dir = os.path.join(os.path.dirname(__file__), 'artifacts')
         artifacts_path = os.path.join(artifacts_dir, self.ARTIFACT_FILENAME)
         contract = ContractGenerator.from_hardhat_artifact(artifacts_path)
         super().__init__(bytecode=contract.bytecode)
-        self._setup(owner_address)
 
-    # private
-
-    def _setup(self, owner_address: str) -> None:
-        self._write_address(0, owner_address)
+    @staticmethod
+    def generate_storage(**kwargs) -> dict:
+        owner_address = kwargs['owner_address']
+        storage = {}
+        ContractGenerator._write_address(
+            storage,
+            ProxyAdminGenerator.OWNER_SLOT,
+            owner_address)
+        return storage
