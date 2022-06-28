387257561d5c8ea3b7f8b5c59f3b17a2dc584c62
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-06-29 00:27:58 +0300

    Fix openzeppelin contract generator

--- src/predeployed_generator/openzeppelin/openzeppelin_contract_generator.py
+++ src/predeployed_generator/openzeppelin/openzeppelin_contract_generator.py
@@ -12,15 +12,21 @@ from ..contract_generator import ContractGenerator
 class OpenzeppelinContractGenerator(ContractGenerator):
     '''Creates contract generator based on hardhat artifact file from `artifacts` folder'''
     ARTIFACT_FILENAME: str = ''
+    META_FILENAME: str = ''
 
     def __init__(self):
         if not self.get_artifact_filename():
             raise TypeError('ARTIFACT_FILENAME is not overloaded in the class')
         artifacts_dir = os.path.join(os.path.dirname(__file__), 'artifacts')
         artifacts_path = os.path.join(artifacts_dir, self.get_artifact_filename())
-        contract = ContractGenerator.from_hardhat_artifact(artifacts_path)
-        super().__init__(bytecode=contract.bytecode, abi=contract.abi)
+        meta_path = os.path.join(artifacts_dir, self.get_meta_filename())
+        contract = ContractGenerator.from_hardhat_artifact(artifacts_path, meta_path)
+        super().__init__(bytecode=contract.bytecode, abi=contract.abi, meta=contract.meta)
 
     def get_artifact_filename(self):
         '''Return filename of contract artifact'''
         return self.ARTIFACT_FILENAME
+
+    def get_meta_filename(self):
+        '''Return filename of contract artifact'''
+        return self.META_FILENAME
